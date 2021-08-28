3a7b0e3e6141a69b0dd7c396860ba61f0e22575b
kavindadilshan <kavindadilshan97@gmail.com>
2021-08-28 15:25:13 +0530

    link react-native-toast-message in app.js

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import AppNavigator from './src/navigator/appNavigator';
-import { AuthProvider } from "./src/context";
+import {AuthProvider} from "./src/context";
+import Toast from 'react-native-toast-message';
 
 
 const App = () => {
-  return (
-    <SafeAreaProvider>
-      <AuthProvider>
-        <AppNavigator/>
-      </AuthProvider>
-    </SafeAreaProvider>  
-  );
+    return (
+        <SafeAreaProvider>
+            <AuthProvider>
+                <AppNavigator/>
+                <Toast ref={(ref) => Toast.setRef(ref)}/>
+            </AuthProvider>
+        </SafeAreaProvider>
+    );
 }
 
 export default App;
