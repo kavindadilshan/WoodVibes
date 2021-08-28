a9c81dcc199f123275c96cdc75551619b2d70b14
kavindadilshan <kavindadilshan97@gmail.com>
2021-08-28 15:47:26 +0530

    created notifyMessage class

--- src/utils/commonFunc.js
+++ src/utils/commonFunc.js
@@ -1,5 +1,30 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
 
-export const clearLocalStorage = async ()=>{
+export const clearLocalStorage = async () => {
     await AsyncStorage.clear();
+}
+
+export const notifyMessage = (msg1, type, msg2) => {
+    let msgType = "info";
+
+    if (type === 2) {
+        msgType = "info"
+    } else if (type === 0) {
+        msgType = "error"
+    } else if (type === 1) {
+        msgType = "success"
+    }
+
+    Toast.show({
+        type: msgType,
+        position: 'top',
+        text1: msg1,
+        text2: msg2,
+        visibilityTime: 4000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+    });
+
 }
