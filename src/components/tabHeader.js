361974f9e433f23e6361335b34fa7f7b6f18048a
kavindadilshan <kavindadilshan97@gmail.com>
2021-09-08 00:36:33 +0530

    added back button

--- src/components/tabHeader.js
+++ src/components/tabHeader.js
@@ -1,35 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import IconI from 'react-native-vector-icons/Ionicons';
 
 import * as Constants from '../utils/constants';
 
-const TabHeader = ({title, rightComponent}) => {
+const TabHeader = ({title, rightComponent,backButton,navigation}) => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.headerText}>{title}</Text>
-            {
-                rightComponent
-            }
+            {backButton&&(
+                <IconI
+                    name='chevron-back-outline'
+                    size={25}
+                    color={Constants.COLORS.BLACK}
+                    style={{marginRight:10}}
+                    onPress={() => navigation.goBack()}
+                />
+            )}
+
+            <View style={styles.subContainer}>
+                <Text style={styles.headerText}>{title}</Text>
+                {
+                    rightComponent
+                }
+            </View>
+
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Constants.COLORS.BACKGROUND_ASH,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 20,
-    marginVertical: 15,
-    alignItems: 'center'
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: Constants.COLORS.BLACK,
-  }
+    container: {
+        backgroundColor: Constants.COLORS.BACKGROUND_ASH,
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        marginVertical: 15,
+        alignItems: 'center',
+    },
+    subContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width:'100%'
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: Constants.COLORS.BLACK,
+    }
 });
 
 export default TabHeader;
