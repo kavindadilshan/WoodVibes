0ce0fd6862981604c57ed257f7eed6a44bcb9dfb
kavindadilshan <kavindadilshan97@gmail.com>
2021-09-07 22:19:04 +0530

    created alert message component

---
+++ src/components/AlertMessage.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import AwesomeAlert from "react-native-awesome-alerts";
+import * as Constance from '../utils/constants';
+
+class App extends React.Component {
+    render() {
+        return (
+            <AwesomeAlert
+                show={this.props.show}
+                showProgress={false}
+                title={this.props.title}
+                message={this.props.message !== undefined ? this.props.message : null}
+                closeOnTouchOutside={this.props.onDismiss !== undefined}
+                closeOnHardwareBackPress={this.props.onDismiss !== undefined}
+                showCancelButton={true}
+                showConfirmButton={true}
+                cancelText={this.props.cancelText}
+                confirmText={this.props.confirmText}
+                confirmButtonColor={Constance.COLORS.WHITE}
+                onDismiss={this.props.onDismiss}
+                onCancelPressed={this.props.onCancelPressed}
+                onConfirmPressed={this.props.onConfirmPressed}
+                confirmButtonTextStyle={{color: Constance.COLORS.BLACK, textAlign: "center"}}
+                titleStyle={{
+                    fontSize: 20,
+                    lineHeight: 22,
+                    color: Constance.COLORS.BLACK,
+                    textAlign: "center",
+                    marginBottom: 10
+                }}
+                messageStyle={{
+                    fontSize: 15,
+                    lineHeight: 22,
+                    color: Constance.COLORS.BLACK,
+                    textAlign: "center"
+                }}
+                cancelButtonColor={Constance.COLORS.RED}
+                contentStyle={{alignItems: 'center', justifyContent: 'center', marginVertical: '5%'}}
+                contentContainerStyle={{borderRadius: 15}}
+                cancelButtonStyle={{
+                    width: 102,
+                    height: 43,
+                    borderRadius: 10,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    elevation: 5,
+                    shadowColor: Constance.COLORS.BLACK,
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 2,
+                }}
+                confirmButtonStyle={{
+                    width: 102,
+                    height: 43,
+                    borderRadius: 10,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    elevation: 5,
+                    shadowColor: Constance.COLORS.BLACK,
+                    shadowOffset: {
+                        width: 0,
+                        height: 1,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 2,
+                }}
+                cancelButtonTextStyle={{textAlign: "center"}}
+            />
+        )
+    }
+}
+
+export default App;
--- src/screens/woodSetup/base.js
+++ src/screens/woodSetup/base.js
@@ -9,16 +9,21 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import {StorageStrings} from "../../utils/constants";
 import * as CustomerServices from "../../services/customer";
 import * as commonFunc from "../../utils/commonFunc";
+import AlertMessage from "../../components/AlertMessage";
+import Loading from "../../components/loading";
 
 const WoodSetupBase = ({navigation}) => {
 
-    const [invoiceList,setInvoiceList]=useState({});
+    const [invoiceList, setInvoiceList] = useState({});
+    const [showAlert, setShowAlert] = useState(false);
+    const [loading,setLoading]=useState(false);
 
     useEffect(async () => {
-        await getAllCustomersList();
+        setLoading(true);
+        await getAllInvoiceList();
     }, [])
 
-    async function getAllCustomersList() {
+    async function getAllInvoiceList() {
         const factoryId = await AsyncStorage.getItem(StorageStrings.FACTORYID);
         await InvoiceServices.getAllInvoice(factoryId)
             .then(response => {
@@ -27,33 +32,49 @@ const WoodSetupBase = ({navigation}) => {
             .catch(error => {
                 commonFunc.notifyMessage(error.message, 0);
             })
+        setLoading(false);
     }
 
-    const addOnPress = () => {
-        // do logic
+    async function deleteInvoice(item) {
+        switch (item) {
+            case 'yes':
+                setShowAlert(true);
+                break;
+            case 'no':
+                setShowAlert(false);
+                break;
+            default:
+                break;
+        }
     }
-
-    const headerRightBtn = <Button
-                                title="Add New"
-                                onPress={addOnPress}
-                                containerStyle={styles.addNewButtonContainerStyle}
-                                buttonStyle={styles.addNewButtonStyle}
-                                titleStyle={styles.addNewButtonTitleStyle}
-                            />
 
     return (
         <View style={styles.container}>
             <TabHeader title='Invoice Details'/>
-            <ScrollView contentContainerStyle={{paddingBottom:10}}>
-                {Object.keys(invoiceList).map((item,i)=>(
+            <ScrollView contentContainerStyle={{paddingBottom: 10}}>
+                {Object.keys(invoiceList).map((item, i) => (
                     <Card containerStyle={styles.listCard} key={i}>
-                        <Card.Title style={styles.listCardTitle}>
-                            Sample Invoice
-                        </Card.Title>
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            marginBottom: 5
+                        }}>
+                            <Card.Title style={styles.listCardTitle}>
+                                Sample Invoice
+                            </Card.Title>
+                            <Button
+                                title="Delete"
+                                onPress={()=>setShowAlert(true)}
+                                containerStyle={styles.addNewButtonContainerStyle}
+                                buttonStyle={styles.addNewButtonStyle}
+                                titleStyle={styles.addNewButtonTitleStyle}
+                            />
+                        </View>
                         <Card.Divider/>
                         <View style={styles.listCardItem}>
                             <Text style={styles.listCardItemHeader}>Total Amount</Text>
-                            <Text style={styles.listCardItemDesc}>{invoiceList[item].totalAmount}</Text>
+                            <Text style={styles.listCardItemDesc}>Rs. {invoiceList[item].totalAmount}</Text>
                         </View>
                         <View style={styles.listCardItem}>
                             <Text style={styles.listCardItemHeader}>Invoice Date</Text>
@@ -63,26 +84,35 @@ const WoodSetupBase = ({navigation}) => {
                 ))}
 
             </ScrollView>
+            <Loading isVisible={loading}/>
+            <AlertMessage
+                show={showAlert}
+                title={"Do you want to delete this record?"}
+                onCancelPressed={() => deleteInvoice('yes')}
+                onConfirmPressed={() => deleteInvoice('no')}
+                cancelText={'Yes'}
+                confirmText={'Not Now'}
+            />
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: Constants.COLORS.BACKGROUND_ASH,
+        flex: 1,
+        backgroundColor: Constants.COLORS.BACKGROUND_ASH,
     },
     addNewButtonContainerStyle: {
-      borderRadius: 10,
-      height: 40,
+        borderRadius: 10,
+        height: 40,
     },
     addNewButtonStyle: {
-      height: 40,
-      paddingHorizontal: 20,
-      backgroundColor: Constants.COLORS.PRIMARY_COLOR
+        height: 40,
+        paddingHorizontal: 20,
+        backgroundColor: Constants.COLORS.PRIMARY_COLOR
     },
     addNewButtonTitleStyle: {
-      fontSize: 16,
+        fontSize: 16,
     },
     listCard: {
         borderRadius: 10,
@@ -90,7 +120,7 @@ const styles = StyleSheet.create({
     },
     listCardTitle: {
         fontSize: 16,
-        textAlign: 'left',
+        textAlign: 'center',
         color: Constants.COLORS.PRIMARY_COLOR,
     },
     listCardItem: {
